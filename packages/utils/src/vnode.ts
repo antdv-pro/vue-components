8322c75797aac3e1054d50490a50cc89d028846c
aibayanyu <aibayanyu@qq.com>
2023-03-18 08:26:51 +0800

    feat: add filterempty

--- packages/utils/src/vnode.ts
+++ packages/utils/src/vnode.ts
@@ -1,5 +1,7 @@
-import type { Slot } from 'vue'
+import type { Slot, VNode, VNodeChild } from 'vue'
+import { Fragment } from 'vue'
 import { isFunction } from './base'
+
 export const getSlotsProps = <S extends object, P extends object, K extends keyof S, KP extends keyof P>(slots: S, props: P, key: K | KP, ...args: any[]) => {
   if (key in slots && isFunction(slots[key as K])) {
     const slot: Slot = slots[key as K] as Slot
@@ -16,4 +18,21 @@ export const getSlotsProps = <S extends object, P extends object, K extends keyo
 export const hasSlotProps = <S extends object, P extends object, K extends keyof S, KP extends keyof P>(slots: S, props: P, key: K | KP) => {
   if (key in slots && slots[key as K]) return true
   return !!(key in props && props[key as KP])
+}
+
+export function isEmptyElement(c: any) {
+  return c && (c.type === Comment || (c.type === Fragment && c.children.length === 0) || (c.type === Text && c.children.trim() === ''))
+}
+
+export function filterEmpty(children: VNodeChild[] = []) {
+  const res: VNodeChild[] = []
+  children.forEach((child) => {
+    if (Array.isArray(child))
+      res.push(...child)
+    else if ((child as any)?.type === Fragment)
+      res.push(...filterEmpty((child as any).children))
+    else
+      res.push(child)
+  })
+  return res.filter(c => !isEmptyElement(c as VNode))
 }
