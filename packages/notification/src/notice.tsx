34677979938c135c427fd6e2c83a5c1c6fa557b5
aibayanyu <aibayanyu@qq.com>
2023-04-20 08:26:29 +0800

    feat: add notification

---
+++ packages/notification/src/notice.tsx
@@ -0,0 +1,114 @@
+import type { VueKey } from '@v-c/utils'
+import {
+  KeyCode,
+  booleanType,
+  classNames,
+  eventType,
+  isFunction,
+  numberType,
+  objectType,
+  omit,
+  someType,
+  stringType, useState, vNodeType,
+} from '@v-c/utils'
+import type { ExtractPropTypes, HTMLAttributes, VNodeChild } from 'vue'
+import { defineComponent, onBeforeUnmount, watch } from 'vue'
+
+export const noticeConfig = {
+  content: vNodeType<VNodeChild | (() => VNodeChild)>(),
+  duration: numberType(),
+  closeIcon: vNodeType<VNodeChild | (() => VNodeChild)>(),
+  closable: booleanType(),
+  /** @private Internal usage. Do not override in your code */
+  props: objectType<HTMLAttributes & Record<string, any>>(),
+  onClose: eventType<VoidFunction>(),
+  onClick: eventType<(e: MouseEvent) => void>(),
+}
+
+export type NoticeConfig = Partial<ExtractPropTypes<typeof noticeConfig>>
+
+export const noticeProps = {
+  ...omit(noticeConfig, ['onClose']),
+  prefixCls: stringType(),
+  eventKey: someType<VueKey>([String, Number]),
+  onClick: eventType<(e: MouseEvent) => void>(),
+  onNoticeClose: eventType<(key: VueKey) => void>(),
+}
+
+export type NoticeProps = ExtractPropTypes<typeof noticeProps>
+
+const Notify = defineComponent({
+  name: 'Notify',
+  props: {
+    ...noticeProps,
+    times: numberType(),
+  },
+  setup(props, { attrs, slots, emit }) {
+    const [hovering, setHovering] = useState(false)
+    // ======================== Close =========================
+    const onInternalClose = () => {
+      props?.onNoticeClose?.(props.eventKey)
+    }
+
+    const onCloseKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Enter' || e.code === 'Enter' || e.keyCode === KeyCode.ENTER)
+        onInternalClose()
+    }
+
+    let timeout: any
+    // ======================== Effect ========================
+    watch([() => props.duration, () => hovering, () => props.times], () => {
+      if (!hovering && props.duration > 0) {
+        timeout = setTimeout(() => {
+          onInternalClose()
+        }, props.duration * 1000)
+      }
+    }, {
+      immediate: true,
+      flush: 'post',
+    })
+    onBeforeUnmount(() => {
+      if (timeout)
+        clearTimeout(timeout)
+    })
+
+    return () => {
+      // ======================== Render ========================
+      const { prefixCls, props: divProps, closable, onClick, content, closeIcon } = props
+      const noticePrefixCls = `${prefixCls}-notice`
+      const divCls = classNames(noticePrefixCls, attrs.class, {
+        [`${noticePrefixCls}-closable`]: closable,
+      })
+      return (
+        <div
+         {...attrs}
+         {...divProps}
+         class={divCls}
+         onMouseenter={() => {
+           setHovering(true)
+         }}
+         onMouseleave={() => {
+           setHovering(false)
+         }}
+         onClick={onClick}
+        >
+            <div class={`${noticePrefixCls}-content`}>{isFunction(content) ? content() : content}</div>
+            {closable && (
+                <a
+                   tabindex={0}
+                   class={`${noticePrefixCls}-close`}
+                   onKeydown={onCloseKeyDown}
+                   onClick={(e) => {
+                     e.preventDefault()
+                     e.stopPropagation()
+                     onInternalClose()
+                   }}
+                >{isFunction(closeIcon) ? closeIcon() : closeIcon}</a>
+            )}
+        </div>
+      )
+    }
+  },
+})
+
+export default Notify
---
+++ packages/notification/src/notifications.tsx
@@ -0,0 +1,145 @@
+import { type VueKey, eventType, numberType, useState } from '@v-c/utils'
+import { someType, stringType, vNodeType } from '@v-c/utils'
+import { type ExtractPropTypes, type VNodeChild, defineComponent, shallowRef, watch } from 'vue'
+import type { CSSMotionProps } from '@v-c/motion'
+import { noticeConfig } from './notice'
+
+export type Placement = 'top' | 'topLeft' | 'topRight' | 'bottom' | 'bottomLeft' | 'bottomRight'
+export const openConfig = {
+  ...noticeConfig,
+  key: someType<VueKey>([String, Number]),
+  placement: stringType<Placement>(),
+  content: vNodeType<VNodeChild | (() => VNodeChild)>(),
+  duration: numberType(),
+}
+
+type Placements = Partial<Record<Placement, OpenConfig[]>>
+
+export type OpenConfig = Partial<ExtractPropTypes<typeof openConfig>>
+
+export const notificationProps = {
+  prefixCls: stringType(),
+  motion: someType<CSSMotionProps | ((placement: Placement) => CSSMotionProps)>([Object, Function]),
+  container: someType<HTMLElement | (() => HTMLElement)>([Object, Function]),
+  maxCount: numberType(),
+  onAllRemoved: eventType<VoidFunction>(),
+}
+
+type InnerOpenConfig = OpenConfig & { times?: number }
+
+export interface NotificationsRef {
+  open: (config: OpenConfig) => void
+  close: (key: VueKey) => void
+  destroy: () => void
+}
+
+const Notification = defineComponent({
+  name: 'Notification',
+  props: notificationProps,
+  setup(props, { attrs, expose, slots, emit }) {
+    const [configList, setConfigList] = useState<OpenConfig[]>([])
+    // ======================== Close =========================
+    const onNoticeClose = (key: VueKey) => {
+      // Trigger close event
+      const config = configList.value.find(item => item.key === key)
+      config?.onClose?.()
+
+      setConfigList(list => list.filter(item => item.key !== key))
+    }
+    // ========================= Refs =========================
+    expose({
+      open: (config: OpenConfig) => {
+        setConfigList((list) => {
+          let clone = [...list]
+          // Replace if exist
+          const index = clone.findIndex(item => item.key === config.key)
+          const innerConfig: InnerOpenConfig = { ...config }
+          if (index >= 0) {
+            innerConfig.times = ((list[index] as InnerOpenConfig)?.times || 0) + 1
+            clone[index] = innerConfig
+          }
+          else {
+            innerConfig.times = 0
+            clone.push(innerConfig)
+          }
+
+          if (props.maxCount > 0 && clone.length > props.maxCount)
+            clone = clone.slice(-props.maxCount)
+
+          return clone
+        })
+      },
+      close: (key: VueKey) => {
+        onNoticeClose(key)
+      },
+      destroy: () => {
+        setConfigList([])
+      },
+    })
+
+    // ====================== Placements ======================
+    const [placements, setPlacements] = useState<Placements>({})
+
+    watch(configList, () => {
+      const nextPlacements: Placements = {}
+
+      configList.value.forEach((config) => {
+        const { placement = 'topRight' } = config
+
+        if (placement) {
+          nextPlacements[placement] = nextPlacements[placement] || []
+          nextPlacements[placement].push(config)
+        }
+      })
+
+      // Fill exist placements to avoid empty list causing remove without motion
+      Object.keys(placements).forEach((placement) => {
+        nextPlacements[placement as Placement] = nextPlacements[placement as Placement] || []
+      })
+
+      setPlacements(nextPlacements)
+    }, { immediate: true, flush: 'post' })
+
+    // Clean up container if all notices fade out
+    const onAllNoticeRemoved = (placement: Placement) => {
+      setPlacements((originPlacements) => {
+        const clone = {
+          ...originPlacements,
+        }
+        const list = clone[placement] || []
+
+        if (!list.length)
+          delete clone[placement]
+
+        return clone
+      })
+    }
+
+    // Effect tell that placements is empty now
+    const emptyRef = shallowRef(false)
+    watch(placements, () => {
+      if (Object.keys(placements).length > 0) {
+        emptyRef.value = true
+      }
+      else if (emptyRef.value) {
+        // Trigger only when from exist to empty
+        props?.onAllRemoved?.()
+        emptyRef.value = false
+      }
+    }, {
+      immediate: true,
+      flush: 'post',
+    })
+
+    return () => {
+      // ======================== Render ========================
+      const { container } = props
+      if (!container)
+        return null
+
+      const placementList = Object.keys(placements.value) as Placement[]
+    }
+  },
+})
+
+export default Notification
