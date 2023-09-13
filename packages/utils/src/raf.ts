6cc038996a9172663a493408d053b1f6948b3b1a
aibayanyu <aibayanyu@qq.com>
2023-09-14 07:48:49 +0800

    feat: add use hook

--- packages/utils/src/raf.ts
+++ packages/utils/src/raf.ts
@@ -1,14 +1,48 @@
-let raf = (callback: FrameRequestCallback) => +setTimeout(callback, 16)
+let raf = (callback: FrameRequestCallback) => setTimeout(callback, 16) as any
 let caf = (num: number) => clearTimeout(num)
 
 if (typeof window !== 'undefined' && 'requestAnimationFrame' in window) {
-  raf = (callback: FrameRequestCallback) =>
-    window.requestAnimationFrame(callback)
+  raf = (callback: FrameRequestCallback) => window.requestAnimationFrame(callback)
   caf = (handle: number) => window.cancelAnimationFrame(handle)
 }
 
-export default function wrapperRaf(callback: () => void): number {
-  return raf(callback)
+let rafUUID = 0
+const rafIds = new Map<number, number>()
+
+function cleanup(id: number) {
+  rafIds.delete(id)
 }
 
-wrapperRaf.cancel = caf
+export default function wrapperRaf(callback: () => void, times = 1): number {
+  rafUUID += 1
+  const id = rafUUID
+
+  function callRef(leftTimes: number) {
+    if (leftTimes === 0) {
+      // Clean up
+      cleanup(id)
+
+      // Trigger
+      callback()
+    }
+    else {
+      // Next raf
+      const realId = raf(() => {
+        callRef(leftTimes - 1)
+      })
+
+      // Bind real raf id
+      rafIds.set(id, realId)
+    }
+  }
+
+  callRef(times)
+
+  return id
+}
+
+wrapperRaf.cancel = (id: number) => {
+  const realId = rafIds.get(id)
+  cleanup(realId as any)
+  return caf(realId as any)
+}
--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsup'
-
+import fg from 'fast-glob'
+const files = fg.sync(['./src/**/*.ts', './src/**/*.tsx', '!src/**/*.d.ts'])
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: [...files],
   format: ['esm', 'cjs'],
   dts: {
     resolve: true,
